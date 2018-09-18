79dee6e2c5dd8a80e63bce853cfb799833c35994
PoojaB26 <pbhaumik26@gmail.com>
2018-09-19 01:41:17 +0530

    added repo cards UI

--- lib/github_home.dart
+++ lib/github_home.dart
@@ -11,7 +11,69 @@ class GithubHome extends StatelessWidget{
         title: Text('Trending Repositories'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        child: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index){
+              return RepoCard();
+            }),
+      ),
+    );
+  }
+
+}
+
+
+class RepoCard extends StatelessWidget{
+
+  _getAvatar(){
+    return CircleAvatar(
+      radius: 20.0,
+      backgroundColor: Colors.grey,
+    );
+  }
+
+  _getRepoInfo(){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        RichText(
+          textAlign: TextAlign.start,
+          text: TextSpan(
+            style: TextStyle(
+              color: Colors.blueGrey,
+              fontSize: 20.0
+            ),
+            children: [
+              TextSpan(text: "google/",
+                  style:TextStyle(fontWeight: FontWeight.bold)),
+              TextSpan(text: "guava")
+            ]
+          ),
+        ),
+        Text("Description Lorem Ipsum")
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
+      child: Card(
+        color: Colors.white,
+        child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                _getAvatar(),
+                SizedBox(width: 10.0,),
+                _getRepoInfo(),
+              ],
+            ),
+        ),
+      ),
     );
   }
 
