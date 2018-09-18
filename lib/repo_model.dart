e03325539982b8340b830bbc1554bef40d3db78a
PoojaB26 <pbhaumik26@gmail.com>
2018-09-19 02:03:27 +0530

    added repo model file

---
+++ lib/repo_model.dart
@@ -0,0 +1,56 @@
+class RepoList{
+  List<RepoListItem> repoList;
+
+  RepoList({
+    this.repoList,
+});
+
+  factory RepoList.fromJson(List<dynamic> json){
+
+    List<RepoListItem> repos = new List<RepoListItem>();
+    repos = json.map((i)=>RepoListItem.fromJson(i)).toList();
+    return RepoList(
+      repoList: repos
+    );
+  }
+
+}
+
+
+class RepoListItem{
+  String username;
+  String avatar;
+  Repo repos;
+
+  RepoListItem({
+    this.username,
+    this.avatar,
+    this.repos,
+  });
+
+  factory RepoListItem.fromJson(Map<String, dynamic> json) {
+
+    return new RepoListItem(
+        username: json['username'],
+        repos: Repo.fromJson(json['repo'])
+    );
+  }
+}
+
+
+class Repo{
+  String name;
+  String description;
+
+  Repo({
+    this.name,
+    this.description
+});
+
+  factory Repo.fromJson(Map<String, dynamic> json){
+    return new Repo(
+      name: json['name'],
+      description: json['description']
+    );
+  }
+}
