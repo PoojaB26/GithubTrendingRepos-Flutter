6c5fbe72ecbcefbb23c77c05b4501f989507eddf
PoojaB26 <pbhaumik26@gmail.com>
2018-09-19 02:45:55 +0530

    partThree branch

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -8,7 +8,7 @@ import 'package:trending_github/repo_model.dart';
 String url = 'https://github-trending-api.now.sh/developers?language=java&since=daily';
 
 
-Future<RepoList> getRepositories() async {
+Future<RepoList> getRepositories() async { 
   final response = await http.get(url,
     headers: {
       HttpHeaders.contentTypeHeader : 'application/json',
